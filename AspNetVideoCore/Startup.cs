f78bd296e2426d635675d6fbebc6658c99d65704
Jonas <N/A>
2017-10-17 11:11:40 +0200

    Compiling the Solution

--- AspNetVideoCore/Startup.cs
+++ AspNetVideoCore/Startup.cs
@@ -27,7 +27,7 @@ namespace AspNetVideoCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello, from My World!");
             });
         }
     }
