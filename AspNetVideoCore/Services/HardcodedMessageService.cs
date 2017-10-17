0b4d43a0f0acf7c2872677df7cb11eb090584f67
Jonas <N/A>
2017-10-17 11:49:41 +0200

    Adding the HardcodedMessageService Class

---
+++ AspNetVideoCore/Services/HardcodedMessageService.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetVideoCore.Services
+{
+    public class HardcodedMessageService : IMessageService
+    {
+        public string GetMessage()
+        {
+            return "Hardcoded message from a service.";
+        }
+    }
+}
