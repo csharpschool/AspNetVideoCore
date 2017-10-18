ae536d3f5eae0b4735841e3732370db0828586ee
Jonas <N/A>
2017-10-18 09:06:49 +0200

    Adding Another Controller

---
+++ AspNetVideoCore/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿namespace AspNetVideoCore.Controllers
+{
+    public class EmployeeController
+    {
+        public string Name()
+        {
+            return "Jonas";
+        }
+
+        public string Country()
+        {
+            return "Sweden";
+        }
+
+        public string Index()
+        {
+            return "Hello from Employee";
+        }
+    }
+}
