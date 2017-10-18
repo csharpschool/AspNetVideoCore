94b3c4d6238663de5520cb97757ca470da7d650e
Jonas <N/A>
2017-10-18 09:22:56 +0200

    Attribute Routing

--- AspNetVideoCore/Controllers/EmployeeController.cs
+++ AspNetVideoCore/Controllers/EmployeeController.cs
@@ -1,17 +1,23 @@
-﻿namespace AspNetVideoCore.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetVideoCore.Controllers
 {
+    [Route("employee")]
     public class EmployeeController
     {
+        [Route("name")]
         public string Name()
         {
             return "Jonas";
         }
 
+        [Route("country")]
         public string Country()
         {
             return "Sweden";
         }
 
+        [Route("")]
         public string Index()
         {
             return "Hello from Employee";
