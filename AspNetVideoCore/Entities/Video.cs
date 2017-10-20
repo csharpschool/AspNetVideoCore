1a22bfc99030d5bfeac6311459876b59d5922fb6
Jonas <N/A>
2017-10-20 12:23:53 +0200

    Data Annotations - Adding Data Annotations in the Video Entity and VideoEditViewModel Class

--- AspNetVideoCore/Entities/Video.cs
+++ AspNetVideoCore/Entities/Video.cs
@@ -1,11 +1,15 @@
 ﻿using AspNetVideoCore.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetVideoCore.Entities
 {
     public class Video
     {
         public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(80)]
+        //[DataType(DataType.Password)]
         public string Title { get; set; }
+        [Display(Name = "Film Genre")]
         public Genres Genre { get; set; }
     }
 }
--- AspNetVideoCore/ViewModels/VideoEditViewModel.cs
+++ AspNetVideoCore/ViewModels/VideoEditViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using AspNetVideoCore.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspNetVideoCore.ViewModels
 {
     public class VideoEditViewModel
     {
         public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(80)]
         public string Title { get; set; }
         public Genres Genre { get; set; }
     }
