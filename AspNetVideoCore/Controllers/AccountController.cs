b74dd540ff984081c9c62d0cbd2a5ac55e5b68dd
Jonas <N/A>
2017-10-22 14:45:18 +0200

    User Registration

---
+++ AspNetVideoCore/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using AspNetVideoCore.Entities;
+using AspNetVideoCore.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetVideoCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) return View();
+
+            var user = new User { UserName = model.Username };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ AspNetVideoCore/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetVideoCore.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required, MaxLength(255)]
+        public string Username { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
