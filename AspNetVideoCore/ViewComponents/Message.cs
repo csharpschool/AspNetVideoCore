a9ea22c234370506b847b1ffedd3a95fad7de799
Jonas <N/A>
2017-10-22 11:54:04 +0200

    View Components

---
+++ AspNetVideoCore/ViewComponents/Message.cs
@@ -0,0 +1,22 @@
+﻿using AspNetVideoCore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetVideoCore.ViewComponents
+{
+    public class Message : ViewComponent
+    {
+        private IMessageService _message;
+
+        public Message(IMessageService message)
+        {
+            _message = message;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _message.GetMessage();
+            return View("Default", model);
+        }
+
+    }
+}
