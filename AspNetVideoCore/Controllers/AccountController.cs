c1b2ee99acfa846fb005c5af6bb216c05fff5287
Jonas <N/A>
2017-10-22 15:29:51 +0200

    Adding the HTTP POST Login Action

--- AspNetVideoCore/Controllers/AccountController.cs
+++ AspNetVideoCore/Controllers/AccountController.cs
@@ -64,5 +64,28 @@ namespace AspNetVideoCore.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Login failed");
+            return View(model);
+        }
+
     }
 }
