cc3462e81040eb18604e7f3bf0c8829ac8a20f66
Jonas <N/A>
2017-10-22 15:24:50 +0200

    Adding the LoginViewModel Class

---
+++ AspNetVideoCore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetVideoCore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [DataType(DataType.Password), Required]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
